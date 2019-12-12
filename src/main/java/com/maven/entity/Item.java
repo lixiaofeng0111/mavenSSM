fe379e8c70ba0496a504172406325ceed794949e
lixiaofeng0111 <2795317854@qq.com>
2019-12-12 11:28:25 +0800

    Initial commit

---
+++ src/main/java/com/maven/entity/Item.java
@@ -0,0 +1,74 @@
+package com.maven.entity;
+
+import java.util.Date;
+
+/**
+ * Title: Item.java
+ * File description:
+ *
+ * @copyright: 2019
+ * @company: tb
+ * @Author: lxf
+ * @version: 1.0
+ * @date 2019年12月12日
+ */
+public class Item {
+/**
+ * Class description:实体类
+ * @Author: lxf
+ */
+    private String id;
+    private String name;
+    private Double price;
+    private String pic;
+    private Date createTime;
+    private String detail;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getPic() {
+        return pic;
+    }
+
+    public void setPic(String pic) {
+        this.pic = pic;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public String getDetail() {
+        return detail;
+    }
+
+    public void setDetail(String detail) {
+        this.detail = detail;
+    }
+}
